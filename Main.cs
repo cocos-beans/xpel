40e4cb20f54f3f32685d26a8056012bb1c42c9ae
Blair Myhre <120442257+cocos-beans@users.noreply.github.com>
2024-03-07 13:54:34 -0600

    Manual Upload Main.cs

---
+++ Main.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+using XPEL.Compiler.Lexer;
+
+namespace XPEL
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {}
+    }
+}
